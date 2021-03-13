6fae76751a1c30df4f0497813945a21a4b2f675a
Zvat Scorobogatov <zvat.333333@gmail.com>
2021-03-13 14:02:05 +0200

    first commit

---
+++ ls2.py
@@ -0,0 +1,17 @@
+a = int(input("Введите ваше число а: "))
+b = int(input("Введите ваше число b: "))
+c = int(input("Введите ваше число c: "))
+
+print("а самое большое число: ", a > b and a > c)
+print("а большое одного из чисел: ", a > b or a > c)
+
+
+#print("a > b: ", a > b)
+#print("a >= b: ", a >= b)
+#print("a < b: ", a < b)
+#print("a <= b: ", a <= b)
+#print("a == b: ", a == b)
+#print("a != b: ", a != b)
+
+#for good working
+end = input("Для выхода из программы нажмите ENTER")

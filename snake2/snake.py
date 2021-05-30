6afd484b0567cf25a02ce2ee393394a835a5275b
Zvat Scorobogatov <zvat.333333@gmail.com>
2021-05-30 18:41:06 +0300

    snake_2

--- snake2/snake.py
+++ snake2/snake.py
@@ -1,23 +1,40 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-from random  import randint
+from random import randint
 
 pygame.init()
 pygame.display.set_caption("Snake")
 
-head = Rect(400, 300, 30, 30)
 screen = pygame.display.set_mode( (800, 600) )
 clock = pygame.time.Clock()
 SPEED = 10
 DIRECTION = [SPEED, 0]
 COLOR = (255, 255, 255)
+GAME_POINTS = 0
+font = pygame.font.SysFont(None, 32)
 
-def rand_color():
-    r = randint(0, 255)
-    g = randint(0, 255)
-    b = randint(0, 255)
-    return (r, g, b)
+def score():
+    global GAME_POINTS, COLOR
+    text = font.render(f'Очки: {GAME_POINTS}', True, COLOR)
+    text_rect = text.get_rect(center=(400, 500))
+    screen.blit(text, text_rect)
+
+def pickup():
+    global apple_rect, head_rect, GAME_POINTS
+
+    if head_rect.colliderect(apple_rect):
+        apple_rect.x = randint(40, 740)
+        apple_rect.y = randint(40, 560)
+        GAME_POINTS += 10
+
+def load_img(src, x, y):
+    image = pygame.image.load(src).convert()
+    image = pygame.transform.scale(image, (30, 30))
+    rect = image.get_rect(center=(x,y))
+    transparent = image.get_at((0, 0))
+    image.set_colorkey(transparent)
+    return image, rect
 
 def move(obj):
     global DIRECTION, SPEED, COLOR, KEYS
@@ -40,20 +57,18 @@ def move(obj):
         DIRECTION = [-SPEED, 0]
 
     if obj.bottom > 600:
-        DIRECTION = [0, -SPEED]
-        COLOR = rand_color()
+        obj.top = 0
     elif obj.top < 0:
-        DIRECTION = [0, SPEED]
-        COLOR = rand_color()
+        obj.bottom = 600
     elif obj.left < 0:
-        DIRECTION = [SPEED, 0]
-        COLOR = rand_color()
+        obj.right = 800
     elif obj.right > 800:
-        DIRECTION = [-SPEED, 0]
-        COLOR = rand_color()
+        obj.left = 0
 
     obj.move_ip(DIRECTION)
 
+head_image, head_rect = load_img('./img/head.png', 400, 300)
+apple_image, apple_rect = load_img('./img/apple.png', 200, 200)
 
 while True:
     screen.fill( (0, 0, 0) )
@@ -63,7 +78,10 @@ while True:
             exit()
 
     KEYS = pygame.key.get_pressed()
-    pygame.draw.rect(screen, COLOR, head)
-    move(head)
+    screen.blit(head_image, head_rect)
+    screen.blit(apple_image, apple_rect)
+    move(head_rect)
+    pickup()
+    score()
     pygame.display.update()
     clock.tick(30)
